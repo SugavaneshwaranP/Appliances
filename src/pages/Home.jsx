b83f15e965cbf1b643fa93fcb4fe825e15ab3c5a
SugavaneshwaranP <sugavanesh08@gmail.com>
2025-07-31 22:15:05 +0530

    add missing line for improved readability in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
+    
     <div className="min-h-screen bg-gradient-to-br from-green-50 via-white to-blue-50 font-sans text-gray-800 overflow-x-hidden">
       
       {/* Navbar */}
